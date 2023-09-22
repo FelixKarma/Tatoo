dcee638b0a601cc1a76bda1e9db0da0216f00679
ouamine59 <mohamed.manseur@gmail.com>
2023-09-22 15:50:11 +0200

    Create menu js

--- JS/header.js
+++ JS/header.js
@@ -1,1 +1,18 @@
-menu = document.getElementById("btn_menu");
+
+let btn_menu = document.getElementById("btn_menu");
+let menu = document.getElementById("menu");
+btn_menu.addEventListener("click", function() {
+    
+    if (menu.style.display === "none") {
+        menu.style.display = "flex";
+        for(let i = 0; i<300;i++){
+            menu.style.height = i+"px";
+        } 
+      } else {
+       menu.style.display = "none";
+      }
+});
+
+
+
+
