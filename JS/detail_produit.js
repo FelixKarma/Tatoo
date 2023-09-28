f72a5041bffd76ecbbd1c75f0a868c496526e53f
ouamine59 <mohamed.manseur@gmail.com>
2023-09-28 11:49:40 +0200

    update detai produit

--- JS/detail_produit.js
+++ JS/detail_produit.js
@@ -124,6 +124,24 @@ function functAddCart(g){
 functAddCart(1);
 functAddCart(2);
 functAddCart(3);
+
+
+(() => {
+  let max_length = 200;
+  let textarea = document.getElementById("user-text");
+  // set attribute max length 
+  textarea.setAttribute("maxlength", max_length);
+
+  textarea.addEventListener("input", function() {
+      let value_length = this.value.length;
+      if (value_length > max_length) {  
+          this.value = this.value.substring(0, max_length);
+          value_length = this.value.length;
+      }  
+      document.getElementById("char-length").innerText = "Characters restant "+(max_length - value_length);
+  });
+
+})();
 // addCart.addEventListener('click',function(){
 //    // ajouter qte dans affichage panier
 //    if(document.location.href.includes('produit1')){
